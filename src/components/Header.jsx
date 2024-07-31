28fb38fae48a9ad0f47d8921f489732ac6d14d04
Pavel <braurs64@gmail.com>
2024-07-31 16:00:48 +0400

    style: upd Logo in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@nextui-org/react";
 
-const logoPath = "/logo.svg";
+const logoPath = "/favicon.svg";
 
 function Header() {
   const scrollToSubscribe = () => {
@@ -11,7 +11,12 @@ function Header() {
 
   return (
     <div className="flex justify-between sticky top-0 py-7 z-50 mx-auto px-5 md:px-10 lg:justify-center lg:py-10 bg-gradient-to-b from-white via-white to-white/90">
-      <img src={logoPath} alt="Logo" />
+      <div className="inline-flex items-center">
+        <img src={logoPath} alt="Logo" className="w-7 lg:w-11" />
+        <p className="font-days-one ml-1 lg:ml-1.5 text-2xl lg:text-4xl">
+          Glokas
+        </p>
+      </div>
       <Button
         radius="full"
         className="font-bold bg-green-350 text-white w-36 h-11 lg:hidden hover:bg-black"
