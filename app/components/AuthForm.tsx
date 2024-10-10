a81bd6b10b2c9269ee6c02bc1f0d452eb9df405b
Pavel <braurs64@gmail.com>
2024-10-10 17:03:02 +0400

    feat: upd AuthForm

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 
-import { Button } from '@nextui-org/button'
+import { Checkbox } from '@nextui-org/react'
 import { Tab, Tabs } from '@nextui-org/tabs'
 
 import { ApiResponse, errorField } from '@/app/types/api'
@@ -21,8 +21,17 @@ import SuccessSignUpForm from './SuccessSignUp'
 
 function AuthForm() {
   const searchParams = useSearchParams()
-  const [isSignupVisible, setIsSignupVisible] = useState(false)
-  const [isLoginVisible, setIsLoginVisible] = useState(false)
+  const [isSignupPasswordVisible, setIsSignupPasswordVisible] = useState(false)
+  const [isSignupRepeatPasswordVisible, setIsSignupRepeatPasswordVisible] =
+    useState(false)
+  const [isLoginPasswordVisible, setIsLoginPasswordVisible] = useState(false)
+
+  const toggleSignupVisibility = () => setIsSignupPasswordVisible((prev) => !prev)
+
+  const toggleSignupRepeatPasswordVisibility = () =>
+    setIsSignupRepeatPasswordVisible((prev) => !prev)
+
+  const toggleLoginPasswordVisibility = () => setIsLoginPasswordVisible((prev) => !prev)
 
   const [loginErrors, setLoginErrors] = useState<{
     message: string
@@ -48,11 +57,8 @@ function AuthForm() {
 
   const { push, refresh } = useRouter()
   const [isSignupSuccess, setIsSignupSuccess] = useState(false)
-
-  const toggleSignupVisibility = () => {
-    setIsSignupVisible((prev) => !prev)
-  }
-  const toggleLoginVisibility = () => setIsLoginVisible((prev) => !prev)
+  const [passwordRepeat, setPasswordRepeat] = useState('')
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
 
   const handleSignup = async () => {
     setSignupErrors({ message: '', fields: [] })
@@ -67,6 +73,16 @@ function AuthForm() {
 
     if (!usernameSignup) {
       errors.push({ field: 'username', message: 'Username is required' })
+    }
+
+    if (passwordSignup !== passwordRepeat) {
+      errors.push({
+        field: 'passwordRepeat',
+        message: 'Passwords do not match',
+      })
+      setPasswordsMatch(false)
+    } else {
+      setPasswordsMatch(true)
     }
 
     if (errors.length > 0) {
@@ -192,62 +208,35 @@ function AuthForm() {
   }
 
   return (
-    <>
-      <div className="z-50 flex h-[90vh] w-[90vw] flex-row overflow-hidden rounded-medium">
-        <div className="flex h-full w-full shrink-0 flex-col items-center bg-background px-5 pb-0 pt-5 md:w-[400px] lg:w-[480px]">
-          <Link href="/" className="mb-10">
+    <div className="flex min-h-screen w-full bg-[#21A5AC]">
+      <div className="ml-auto flex w-full flex-col justify-center bg-background md:w-[44%]">
+        <div className="mx-auto mt-20 flex h-full w-full flex-col items-center overflow-hidden px-5 md:mt-48 md:w-[54%] md:px-0">
+          <Link href="/" className="mb-20">
             <Image
               src="/images/glokas-logo.svg"
               alt="glokas logo"
-              width={181}
-              height={45}
+              width={341}
+              height={59}
             />
           </Link>
-          <div className="flex w-full flex-col overflow-hidden">
+          <div className="flex w-full flex-grow flex-col overflow-hidden">
             <Tabs
               variant="underlined"
               classNames={{
                 tabList:
-                  'w-full flex-shrink-0 flex-grow-0 basis-auto p-0 px-6 shadow-[inset_0px_-1px_0px_0px_hsl(var(--nextui-secondary-100))]',
-                cursor: 'bg-primary h-px',
-                tab: 'pb-7 font-medium text-lg h-auto data-[focus-visible=true]:outline-0',
-                tabContent: 'text-secondary-200',
-                panel:
-                  'shadow-none scroll-gutter flex-auto overflow-auto pb-10 pl-5 pr-3.5',
-                base: 'mx-5',
+                  'w-full flex-shrink-0 flex-grow-0 gap-0 basis-auto p-0 shadow-[inset_0px_-1px_0px_0px_hsl(var(--nextui-secondary-100))]',
+                cursor: 'bg-primary w-full h-px',
+                tab: 'pb-2 font-medium text-small h-auto data-[focus-visible=true]:outline-0',
+                tabContent: 'text-[#888888] group-data-[selected=true]:text-primary',
+                panel: 'shadow-none px-0 scroll-gutter flex-auto pt-10 pb-16',
+                base: '',
               }}
             >
               <Tab key="sign-up" title="Sign up">
-                <Button
-                  variant="bordered"
-                  color="default"
-                  size="lg"
-                  className="relative mb-2 mt-5 h-14 border-1 hover:shadow-medium"
-                  fullWidth
-                  radius="full"
-                  onClick={() =>
-                    push(
-                      '/api/auth/google?redirectedFrom=' +
-                        (searchParams.get('redirectedFrom') ?? '/')
-                    )
-                  }
-                >
-                  <Image
-                    src="/icons/google.svg"
-                    alt="google"
-                    width={23}
-                    height={22}
-                    className="absolute left-6 md:left-12"
-                  />
-                  Continue with Google
-                </Button>
-                <div className="divider my-5 flex items-center text-center before:mr-7 after:ml-7">
-                  <span className="text-medium font-medium text-secondary">or</span>
-                </div>
                 <label
                   className={`mb-2 text-medium font-medium ${signupErrors?.fields.some((error) => error.field === 'username') ? 'text-danger' : ''}`}
                 >
-                  *Username
+                  Username
                 </label>
                 <CustomInput
                   variant="bordered"
@@ -262,11 +251,22 @@ function AuthForm() {
                   errorMessage={signupErrors?.fields
                     .filter((error) => error.field === 'username')
                     .map((error) => <p key={error.message}>{error.message}</p>)}
+                  endContent={
+                    signupErrors?.fields.some((error) => error.field === 'username') && (
+                      <Image
+                        src={'/icons/warning.svg'}
+                        alt="warning"
+                        width={18}
+                        height={18}
+                        className="mr-2 h-[38px] w-[38px] p-2"
+                      />
+                    )
+                  }
                 />
                 <label
                   className={`mb-2 text-medium font-medium ${signupErrors?.fields.some((error) => error.field === 'email') ? 'text-danger' : ''}`}
                 >
-                  *Email
+                  Email
                 </label>
                 <CustomInput
                   type="email"
@@ -282,11 +282,22 @@ function AuthForm() {
                   errorMessage={signupErrors?.fields
                     .filter((error) => error.field === 'email')
                     .map((error) => <p key={error.message}>{error.message}</p>)}
+                  endContent={
+                    signupErrors?.fields.some((error) => error.field === 'email') && (
+                      <Image
+                        src={'/icons/warning.svg'}
+                        alt="warning"
+                        width={18}
+                        height={18}
+                        className="mr-2 h-[38px] w-[38px] p-2"
+                      />
+                    )
+                  }
                 />
                 <label
                   className={`mb-2 text-medium font-medium ${signupErrors?.fields.some((error) => error.field === 'password') ? 'text-danger' : ''}`}
                 >
-                  *Password
+                  Password
                 </label>
                 <CustomInput
                   variant="bordered"
@@ -294,12 +305,25 @@ function AuthForm() {
                   value={passwordSignup}
                   onChange={(e) => setPasswordSignup(e.target.value)}
                   endContent={
-                    <EyePassword
-                      isVisible={isSignupVisible}
-                      toggleVisibility={toggleSignupVisibility}
-                    />
+                    <>
+                      <EyePassword
+                        isVisible={isSignupPasswordVisible}
+                        toggleVisibility={toggleSignupVisibility}
+                      />
+                      {signupErrors?.fields.some(
+                        (error) => error.field === 'password'
+                      ) && (
+                        <Image
+                          src={'/icons/warning.svg'}
+                          alt="warning"
+                          width={18}
+                          height={18}
+                          className="mr-2 h-[38px] w-[38px] p-2 pl-0"
+                        />
+                      )}
+                    </>
                   }
-                  type={isSignupVisible ? 'text' : 'password'}
+                  type={isSignupPasswordVisible ? 'text' : 'password'}
                   isRequired
                   isInvalid={signupErrors?.fields.some(
                     (error) => error.field === 'password'
@@ -308,42 +332,74 @@ function AuthForm() {
                     .filter((error) => error.field === 'password')
                     .map((error) => <p key={error.message}>{error.message}</p>)}
                 />
-                <div className="mb-5">
-                  <PasswordStrengthMeter password={passwordSignup} />
-                </div>
+
+                <label
+                  className={`mb-2 text-medium font-medium ${
+                    !passwordsMatch ||
+                    signupErrors?.fields.some((error) => error.field === 'passwordRepeat')
+                      ? 'text-danger'
+                      : ''
+                  }`}
+                >
+                  Repeat Password
+                </label>
+                <CustomInput
+                  variant="bordered"
+                  size="sm"
+                  value={passwordRepeat}
+                  onChange={(e) => setPasswordRepeat(e.target.value)}
+                  endContent={
+                    <>
+                      <EyePassword
+                        isVisible={isSignupRepeatPasswordVisible}
+                        toggleVisibility={toggleSignupRepeatPasswordVisibility}
+                      />
+                      {(!passwordsMatch ||
+                        signupErrors?.fields.some(
+                          (error) => error.field === 'passwordRepeat'
+                        )) && (
+                        <Image
+                          src={'/icons/warning.svg'}
+                          alt="warning"
+                          width={18}
+                          height={18}
+                          className="mr-2 h-[38px] w-[38px] p-2 pl-0"
+                        />
+                      )}
+                    </>
+                  }
+                  type={isSignupRepeatPasswordVisible ? 'text' : 'password'}
+                  isRequired
+                  isInvalid={
+                    !passwordsMatch ||
+                    signupErrors?.fields.some((error) => error.field === 'passwordRepeat')
+                  }
+                  errorMessage={
+                    !passwordsMatch
+                      ? 'Passwords do not match'
+                      : signupErrors?.fields
+                          .filter((error) => error.field === 'passwordRepeat')
+                          .map((error) => <p key={error.message}>{error.message}</p>)
+                  }
+                />
+
+                {passwordSignup.length > 0 && (
+                  <div className="mb-5">
+                    <PasswordStrengthMeter password={passwordSignup} />
+                  </div>
+                )}
                 {signupErrors?.message && (
                   <p className="my-4 text-small text-danger">{signupErrors.message}</p>
                 )}
                 <CustomButton fullWidth onClick={handleSignup} isLoading={loading}>
-                  {'Join'}
+                  {'Sign up'}
                 </CustomButton>
               </Tab>
               <Tab key="log-in" title="Log in">
-                <Button
-                  variant="bordered"
-                  color="default"
-                  size="lg"
-                  className="relative mb-2 mt-5 h-14 border-1 hover:shadow-medium"
-                  fullWidth
-                  radius="full"
-                  onClick={() => push('/api/auth/google')}
-                >
-                  <Image
-                    src="/icons/google.svg"
-                    alt="google"
-                    width={23}
-                    height={22}
-                    className="absolute left-6 md:left-12"
-                  />
-                  Continue with Google
-                </Button>
-                <div className="divider my-5 flex items-center text-center before:mr-7 after:ml-7">
-                  <span className="text-medium font-medium text-secondary">or</span>
-                </div>
                 <label
                   className={`mb-2 text-medium font-medium ${loginErrors?.fields.some((error) => error.field === 'email') ? 'text-danger' : ''}`}
                 >
-                  *Email
+                  Username or email
                 </label>
                 <CustomInput
                   type="email"
@@ -357,11 +413,22 @@ function AuthForm() {
                   errorMessage={loginErrors?.fields
                     .filter((error) => error.field === 'email')
                     .map((error) => <p key={error.message}>{error.message}</p>)}
+                  endContent={
+                    loginErrors?.fields.some((error) => error.field === 'email') && (
+                      <Image
+                        src={'/icons/warning.svg'}
+                        alt="warning"
+                        width={18}
+                        height={18}
+                        className="mr-2 h-[38px] w-[38px] p-2"
+                      />
+                    )
+                  }
                 />
                 <label
                   className={`mb-2 text-medium font-medium ${loginErrors?.fields.some((error) => error.field === 'password') ? 'text-danger' : ''}`}
                 >
-                  *Password
+                  Password
                 </label>
                 <CustomInput
                   variant="bordered"
@@ -369,12 +436,25 @@ function AuthForm() {
                   value={passwordLogin}
                   onChange={(e) => setPasswordLogin(e.target.value)}
                   endContent={
-                    <EyePassword
-                      isVisible={isLoginVisible}
-                      toggleVisibility={toggleLoginVisibility}
-                    />
+                    <>
+                      <EyePassword
+                        isVisible={isLoginPasswordVisible}
+                        toggleVisibility={toggleLoginPasswordVisibility}
+                      />
+                      {loginErrors?.fields.some(
+                        (error) => error.field === 'password'
+                      ) && (
+                        <Image
+                          src={'/icons/warning.svg'}
+                          alt="warning"
+                          width={18}
+                          height={18}
+                          className="mr-2 h-[38px] w-[38px] p-2 pl-0"
+                        />
+                      )}
+                    </>
                   }
-                  type={isLoginVisible ? 'text' : 'password'}
+                  type={isLoginPasswordVisible ? 'text' : 'password'}
                   isRequired
                   isInvalid={loginErrors?.fields.some(
                     (error) => error.field === 'password'
@@ -387,9 +467,20 @@ function AuthForm() {
                 {loginErrors?.message && (
                   <p className="my-4 text-small text-danger">{loginErrors.message}</p>
                 )}
+
+                <Checkbox
+                  size="sm"
+                  radius="lg"
+                  className="mb-6 font-medium"
+                  classNames={{ label: 'text-medium' }}
+                >
+                  Remember me
+                </Checkbox>
+
                 <CustomButton fullWidth onClick={handleLogin} isLoading={loading}>
-                  {'Login'}
+                  {'Sign up'}
                 </CustomButton>
+
                 <CustomLink
                   as={Link}
                   color="primary"
@@ -403,12 +494,8 @@ function AuthForm() {
             </Tabs>
           </div>
         </div>
-        <div
-          className="hidden h-full w-full bg-cover bg-center md:block"
-          style={{ backgroundImage: 'url(/ship.webp)' }}
-        ></div>
       </div>
-    </>
+    </div>
   )
 }
 
