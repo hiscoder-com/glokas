e1e770522c7551e90b8f3f5c776ff2723d7407ae
DenisArger <den.arger@gmail.com>
2024-10-14 12:50:52 +0300

    feat: add fontFamily roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   darkMode: 'class',
   plugins: [
