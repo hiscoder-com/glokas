fa6e1b55e54e75ecfe27856869f7ec0a07c13d8c
DenisArger <den.arger@gmail.com>
2024-10-15 13:54:34 +0300

    fix: edit style

--- app/components/YouTubeCard.jsx
+++ app/components/YouTubeCard.jsx
@@ -2,17 +2,19 @@ import Image from 'next/image'
 
 const YouTubeCard = () => {
   return (
-    <div className="flex h-64 w-64 flex-col items-center justify-center rounded-3xl border border-[#C9DFFC] bg-[#EFF5FE] shadow-lg">
-      <div className="flex flex-col items-center justify-center gap-2">
+    <div
+      className="flex h-64 w-64 flex-col items-center justify-center rounded-mega border border-cornflowerBlue-200 bg-cornflowerBlue-50" // prettier-ignore
+    >
+      <div className="flex flex-col items-center justify-center gap-6">
         <Image
           src="/icons/youtube.svg"
           alt="YouTube"
-          width={77}
-          height={77}
+          width={65}
+          height={47}
           priority={true}
         />
         <p
-          className="mt-4 font-roboto text-3xl font-normal" // prettier-ignore
+          className="font-roboto text-xxxxlarge font-normal" // prettier-ignore
         >
           YouTube
         </p>
