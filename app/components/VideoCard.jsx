094bfb5462d1a99966ee67e0f4e127d66b1919b5
DenisArger <den.arger@gmail.com>
2024-10-16 15:33:18 +0300

    style: add breakerBay style

--- app/components/VideoCard.jsx
+++ app/components/VideoCard.jsx
@@ -8,15 +8,41 @@ export default function VideoCard() {
   const fullText = `Jesus Film Project is working on a new, animated family film called JESUS.
   The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
   using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages! Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages!
+  Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages! 
+  Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages!
+  Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages!
+  Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
+  eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages!
+  Jesus Film Project is working on a new, animated family film called JESUS.
+  The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
+  using much of its dialogue but in a refreshed script. This means that this new animated film will 
   eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages!`
+
+  const maxChars = 400
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen)
   }
+
   return (
-    <div className="mx-auto max-h-[230px] max-w-[1600px]">
+    <div>
       <div
-        className="flex flex-col rounded-2xl border bg-breakerBay-50 border-breakerBay-300 p-4 shadow-md md:flex-row" // prettier-ignore
+        className="flex flex-col rounded-xxxxlarge border bg-tertiary-50 border-tertiary-300 px-[26px] py-6 shadow-md md:flex-row" // prettier-ignore
       >
         <div className="md:w-[50%]">
           <Image
@@ -24,17 +50,17 @@ export default function VideoCard() {
             alt="Film Image"
             width={288}
             height={180}
-            className="rounded-lg"
+            className="rounded-large"
           />
         </div>
         <div className="flex flex-col justify-between pl-2">
-          <div className="ml-5 flex flex-col gap-8 md:flex-row">
+          <div className="flex flex-col gap-8 md:flex-row">
             <div className="w-[30%]">
-              <h2 className="mb-1 text-3xl font-bold">
+              <h2 className="mb-1 text-xxxxlarge font-bold text-black-950">
                 Introducing JESUS: A new, animated family film
               </h2>
-              <p className="mb-1 text-lg text-gray-600">Language: English</p>
-              <div className="mt-10 flex items-center gap-1">
+              <p className="mb-1 text-large text-black-900">Language: English</p>
+              <div className="mt-5 flex items-center gap-1">
                 <Image
                   src="icons/youtube.svg"
                   alt="YouTube"
@@ -42,29 +68,23 @@ export default function VideoCard() {
                   height={23}
                   className="text-red-600"
                 />
-                <span className="text-2xl font-medium text-gray-700">YouTube</span>
+                <span className="text-xxlarge font-medium text-black-950">YouTube</span>
               </div>
             </div>
-
             <div className="w-[45%]">
-              <p className="gap-1 text-base text-black">
-                Jesus Film Project is working on a new, animated family film called JESUS.
-                The film will be anchored in the Christian Gospels. It will re-imagine the
-                original JESUS film, using much of its dialogue but in a refreshed script.
-                This means that this new animated film will eventually be available in
-                2,000+ heart languages! 2,000+ heart languages 2,000+ heart languages
-                2,000+ heart languages!2,...
+              <p className="gap-1 text-medium text-black-950">
+                {fullText.slice(0, maxChars) + '...'}
               </p>
               <button
                 onClick={toggleModal}
-                className="mt-2 text-breakerBay-700 underline focus:outline-none"
+                className="mt-2 text-tertiary-700 underline focus:outline-none"
               >
                 View All
               </button>
             </div>
 
             <div className="w-[20%] flex-1">
-              <div className="gap-3 text-lg text-black">
+              <div className="gap-3 text-large text-black-950">
                 <p>Words: 88</p>
                 <p>Languages: 0</p>
                 <p>Last update: 12.12.2024</p>
@@ -74,8 +94,8 @@ export default function VideoCard() {
         </div>
       </div>
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center gap-10 bg-black bg-opacity-50">
-          <div className="relative h-[780px] w-[800px] overflow-y-auto rounded-lg bg-white p-14 shadow-lg md:w-1/2">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative h-[780px] max-h-[780px] min-h-[500px] w-[800px] overflow-y-auto rounded-large bg-white px-16 py-[52px] shadow-lg md:w-1/2">
             <button
               onClick={toggleModal}
               className="absolute right-4 top-4 focus:outline-none"
@@ -85,24 +105,13 @@ export default function VideoCard() {
                 alt="Close"
                 width={24}
                 height={24}
-                className="text-gray-700"
+                className="text-black-700"
               />
             </button>
-            <h3 className="mb-4 text-center text-4xl font-bold">
+            <h3 className="mb-[38px] text-center text-mega font-bold">
               Video Description in English
             </h3>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
-            <p className="mb-4 text-base text-gray-700">{fullText}</p>
+            <p className="mb-4 text-medium text-black-950">{fullText}</p>
           </div>
         </div>
       )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        xlarge: '1.25rem', // text-xlarge (20px)
+        xxlarge: '1.5rem', // text-xxlarge (24px)
+        xxxlarge: '1.75rem', // text-xxxlarge (28px)
+        xxxxlarge: '2rem', // text-xxxxlarge (32px)
+        mega: '2.625rem', // text-mega (42px)
+      },
+      borderRadius: {
+        xlarge: '10px', // rounded-xlarge
+        xxxxlarge: '18px', // rounded-xxxxlarge
+        mega: '2rem', // rounded-mega
+      },
+    },
   },
   darkMode: 'class',
   plugins: [
@@ -59,7 +75,22 @@ module.exports = {
               foreground: '#FFFFFF',
               DEFAULT: '#0ead69',
             },
-            breakerBay: {
+            black: {
+              '50': '#f6f6f6',
+              '100': '#e7e7e7',
+              '200': '#d1d1d1',
+              '300': '#b0b0b0',
+              '400': '#888888',
+              '500': '#6d6d6d',
+              '600': '#5d5d5d',
+              '700': '#4f4f4f',
+              '800': '#454545',
+              '900': '#3d3d3d',
+              '950': '#000000',
+              foreground: '#FFFFFF',
+              DEFAULT: '#6d6d6d',
+            },
+            tertiary: {
               '50': '#effcfb',
               '100': '#d6f7f7',
               '200': '#b1eff0',
