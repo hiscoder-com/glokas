7179dabe0a02d8df2f8144021543facc7407f5f4
Pavel <braurs64@gmail.com>
2024-10-17 10:39:16 +0400

    feat: upd ResetPassword

--- app/(full-width)/reset-password/page.js
+++ app/(full-width)/reset-password/page.js
@@ -1,20 +1,18 @@
-// import { headers } from 'next/headers'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 import ResetForm from '@/app/components/ResetForm'
 
-// import { redirect } from 'next/navigation'
+export default async function ResetPassword({ searchParams }) {
+  const headersList = headers()
+  const host = headersList.get('host')
+  const protocol = headersList.get('x-forwarded-proto') || 'http'
+  const origin = `${protocol}://${host}`
+  const code = searchParams['code'] ?? false
 
-export default async function ResetPassword() {
-  // export default async function ResetPassword({ searchParams }) {
-  // const headersList = headers()
-  // const host = headersList.get('host')
-  // const protocol = headersList.get('x-forwarded-proto') || 'http'
-  // const origin = `${protocol}://${host}`
-  // const code = searchParams['code'] ?? false
-
-  // if (!code) {
-  //   return redirect(`${origin}/auth-error`)
-  // }
+  if (!code) {
+    return redirect(`${origin}/auth-error`)
+  }
 
   return <ResetForm />
 }
