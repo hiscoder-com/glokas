42954c791ad97a14e1ed668228381c48c3ee3b46
DenisArger <den.arger@gmail.com>
2024-10-18 09:12:08 +0300

    feat: add  onSelectionChange in  LanguageSelector

--- app/components/LanguageSelector.jsx
+++ app/components/LanguageSelector.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+
 import { CustomButton } from './CustomButton'
 import CustomSelectSearch from './CustomSelectSearch'
 
 const LanguageSelector = ({ languages }) => {
+  const [hasSelectedLanguages, setHasSelectedLanguages] = useState(false)
+
+  const handleLanguageSelectChange = (selectedItems) => {
+    setHasSelectedLanguages(selectedItems.length > 0)
+  }
+
   return (
     <div className="flex w-full items-start justify-between">
       <div className="flex items-center gap-4">
@@ -9,13 +17,14 @@ const LanguageSelector = ({ languages }) => {
           items={languages}
           placeholder="Select a language"
           displayKey="name"
+          onSelectionChange={handleLanguageSelectChange}
         />
-        <CustomButton color="secondary" size="lg">
+        <CustomButton color="primary" size="lg" isDisabled={!hasSelectedLanguages}>
           Translate
         </CustomButton>
       </div>
       <div>
-        <CustomButton color="primary" variant="bordered" size="lg">
+        <CustomButton color="primary" variant="bordered" size="lg" isDisabled>
           Save
         </CustomButton>
       </div>
