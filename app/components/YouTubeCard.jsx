231c31262f88ff80af9afc49e0fd8af9e7ca43ab
DenisArger <den.arger@gmail.com>
2024-10-18 16:35:31 +0300

    fix: delete div and flex

--- app/components/YouTubeCard.jsx
+++ app/components/YouTubeCard.jsx
@@ -2,17 +2,15 @@ import Image from 'next/image'
 
 const YouTubeCard = () => {
   return (
-    <button className="flex h-64 w-64 cursor-pointer flex-col items-center justify-center rounded-mega border border-primary-200 bg-primary-50 transition-colors duration-300 hover:bg-primary-100 focus:outline-none active:bg-primary-200">
-      <div className="flex flex-col items-center justify-center gap-6">
-        <Image
-          src="/icons/youtube.svg"
-          alt="YouTube"
-          width={65}
-          height={47}
-          priority={true}
-        />
-        <p className="font-roboto text-3xl font-normal">YouTube</p>
-      </div>
+    <button className="flex h-64 w-64 cursor-pointer flex-col items-center justify-center gap-6 rounded-mega border border-primary-200 bg-primary-50 transition-colors duration-300 hover:bg-primary-100 focus:outline-none active:bg-primary-200">
+      <Image
+        src="/icons/youtube.svg"
+        alt="YouTube"
+        width={65}
+        height={47}
+        priority={true}
+      />
+      <p className="font-roboto text-3xl font-normal">YouTube</p>
     </button>
   )
 }
