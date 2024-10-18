b18a42526aed013db452281706459fb0a5df0a3d
valyukhov <alexzed@bk.ru>
2024-10-18 15:18:13 +0300

    feat: :sparkles: refactored

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -1,20 +1,15 @@
-import React from 'react'
-
-// import { getUser } from '../actions/getUser'
 import CookiesBanner from '../components/CookiesBanner'
 import { HeaderBar } from '../components/HeaderBar'
-import { NavigationSidebar } from '../components/NavigationSidebar'
+import { Sidebar } from '../components/Sidebar'
 
 export default async function RootLayout(props: {
   children: React.ReactNode
 }): Promise<JSX.Element> {
   const { children } = props
 
-  // const { user } = await getUser()
-
   return (
     <>
-      <NavigationSidebar user={{ username: 'Alex' }} />
+      <Sidebar />
       <HeaderBar />
       <main className="ml-64 mt-16 overflow-y-auto">{children}</main>
       <CookiesBanner />
--- app/components/HeaderBar.jsx
+++ app/components/HeaderBar.jsx
@@ -1,5 +1,10 @@
 export const HeaderBar = () => {
+  const user = {
+    username: 'VeryVeryLongUsernameInGlokas',
+  }
   return (
-    <div className="fixed left-64 z-10 h-16 w-[calc(100vw-16rem)] border border-primary-100 bg-primary-50"></div>
+    <div className="fixed left-64 z-10 flex h-16 w-[calc(100vw-16rem)] items-center justify-end border border-primary-100 bg-primary-50 pr-8 text-base text-primary-900">
+      {user.username}
+    </div>
   )
 }
