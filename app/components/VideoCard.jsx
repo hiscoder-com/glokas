4fe3fb73739336ed01a96763b3d24308311cecef
DenisArger <den.arger@gmail.com>
2024-10-21 16:21:29 +0300

    feat: add Tooltip

--- app/components/VideoCard.jsx
+++ app/components/VideoCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import Image from 'next/image'
 
@@ -7,8 +7,10 @@ import VideoDescription from './VideoDescription'
 
 export default function VideoCard() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isWarningTooltipVisible, setIsWarningTooltipVisible] = useState(false)
+  const tooltipRef = useRef(null)
 
-  const fullText = `Jesus Film Project is working on a new, animated family film called JESUS.
+  const fullText = `Jesus Film Project is working on a new, animated family film called JESUS. 
   The film will be anchored in the Christian Gospels. It will re-imagine the original "JESUS" film, 
   using much of its dialogue but in a refreshed script. This means that this new animated film will 
   eventually be available in 2,000+ heart languages! 2,000+ heart languages! 2,000+ heart languages! Jesus Film Project is working on a new, animated family film called JESUS.
@@ -44,6 +46,28 @@ export default function VideoCard() {
     const lineCount = text.split('\n').length
     return lineCount < 6
   }
+
+  const toggleWarningTooltip = () => {
+    setIsWarningTooltipVisible(!isWarningTooltipVisible)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (tooltipRef.current && !tooltipRef.current.contains(event.target)) {
+        setIsWarningTooltipVisible(false)
+      }
+    }
+
+    if (isWarningTooltipVisible) {
+      document.addEventListener('mousedown', handleClickOutside)
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isWarningTooltipVisible])
 
   return (
     <div>
@@ -95,12 +119,30 @@ export default function VideoCard() {
                 </button>
               )}
             </div>
-            <div className="w-[20%] flex-1">
-              <div className="gap-3 text-lg text-black-950">
+            <div className="relative w-[20%] flex-1">
+              <div className="flex items-center gap-1 text-lg text-black-950">
                 <p>Words: 88</p>
-                <p>Languages: 0</p>
-                <p>Last update: 12.12.2024</p>
+                <Image
+                  src="icons/exclamation.svg"
+                  alt="Exclamation"
+                  width={20}
+                  height={20}
+                  className="cursor-pointer"
+                  onClick={toggleWarningTooltip}
+                  style={{ filter: 'invert(0)' }}
+                />
+                {isWarningTooltipVisible && (
+                  <div
+                    ref={tooltipRef}
+                    className="absolute bottom-full left-[-26px] z-10 mb-2 w-[240px] translate-x-0 rounded-lg border border-gray-300 bg-white p-4 text-center text-sm text-black-950 shadow-lg"
+                  >
+                    The total word count number includes title and description
+                    <div className="absolute left-1/2 top-full h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-x-transparent border-t-white" />
+                  </div>
+                )}
               </div>
+              <p>Languages: 0</p>
+              <p>Last update: 12.12.2024</p>
             </div>
           </div>
         </div>
