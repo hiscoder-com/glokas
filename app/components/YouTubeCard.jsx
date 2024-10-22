16fbb8a37e307ccf627a3a50d2efd77deb40c42c
DenisArger <den.arger@gmail.com>
2024-10-22 22:04:23 +0300

    fix: delete transition-color

--- app/components/YouTubeCard.jsx
+++ app/components/YouTubeCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const YouTubeCard = () => {
   return (
-    <button className="flex h-64 w-64 cursor-pointer flex-col items-center justify-center gap-6 rounded-3xl border border-primary-200 bg-primary-50 transition-colors duration-300 hover:bg-primary-100 focus:outline-none active:bg-primary-200">
+    <button className="flex h-64 w-64 cursor-pointer flex-col items-center justify-center gap-6 rounded-3xl border border-primary-200 bg-primary-50 duration-300 hover:bg-primary-100 focus:outline-none active:bg-primary-200">
       <Image
         src="/icons/youtube.svg"
         alt="YouTube"
