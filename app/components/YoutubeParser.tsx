d0798f5c2e540f8a4cb4e0f9f542790b08f6cf56
DenisArger <den.arger@gmail.com>
2024-10-22 12:21:52 +0300

    fix: delete button close

--- app/components/YoutubeParser.tsx
+++ app/components/YoutubeParser.tsx
@@ -36,7 +36,7 @@ const YoutubeParser = ({ closeModal }: YoutubeParserProps) => {
   return (
     <>
       <Modal closeModal={closeModal}>
-        <div className="relative max-h-[90vh] w-full max-w-3xl overflow-y-auto rounded-2xl bg-secondary-foreground px-16 pb-16 pt-[52px]">
+        <div className="relative w-full max-w-3xl rounded-2xl bg-secondary-foreground px-16 pb-16 pt-[52px]">
           <div className="mb-12 flex items-center justify-center gap-3">
             <Image
               src="/icons/youtube.svg"
@@ -78,25 +78,12 @@ const YoutubeParser = ({ closeModal }: YoutubeParserProps) => {
             </div>
           </div>
 
-          <div className="max-h-32 overflow-y-auto rounded-xlarge bg-tertiary-100 p-4 text-medium text-tertiary-700 md:text-large">
+          <div className="rounded-xlarge bg-tertiary-100 p-4 text-medium text-tertiary-700 md:text-large">
             Helper text: Lorem ipsum dolor sit amet consectetur. Id enim eu maecenas at.
             Tortor diam nisl eu suspendisse eros scelerisque. Elementum et neque viverra
             ipsum faucibus. Porttitor et nisi aenean id dui risus quis nunc ut.
           </div>
         </div>
-
-        <button
-          className="absolute right-4 top-4 text-gray-500 hover:text-gray-700"
-          onClick={closeModal}
-        >
-          <Image
-            src="/icons/close.svg"
-            alt="Close"
-            width={24}
-            height={24}
-            priority={true}
-          />
-        </button>
       </Modal>
     </>
   )
