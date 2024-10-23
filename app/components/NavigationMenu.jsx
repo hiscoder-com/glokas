4d4f5a78d3fb85739cd902cd7470d149cc33b192
valyukhov <alexzed@bk.ru>
2024-10-24 01:58:06 +0300

    fix: :bug: fix default open accordion

--- app/components/NavigationMenu.jsx
+++ app/components/NavigationMenu.jsx
@@ -20,9 +20,11 @@ export const NavigationMenu = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const [currentPage, setCurrentPage] = useState('')
+  const [isMenuReady, setIsMenuReady] = useState(false)
   useEffect(() => {
     const page = searchParams.get('page')
     setCurrentPage(page || '')
+    setIsMenuReady(true)
   }, [searchParams])
 
   const isYoutubePage = useMemo(() => currentPage === 'youtube', [currentPage])
@@ -39,49 +41,59 @@ export const NavigationMenu = () => {
     }
   }
   return (
-    <ul className="flex flex-col text-sm">
-      <li>
-        <Accordion
-          className="px-0"
-          itemClasses={{
-            base: 'data-[open=true]:bg-primary-100 rounded-medium px-0 py-2',
-          }}
-          defaultExpandedKeys={isYoutubePage || isAllVideoPage ? ['1'] : []}
-        >
-          <AccordionItem
-            classNames={{
-              indicator: 'text-lg text-black-950 -rotate-90 data-[open=true]:rotate-90',
-              trigger: `gap-1 w-full rounded-medium data-[open=true]:rounded-none p-2 ${isYoutubePage ? 'bg-primary-200' : ''}`,
-              content: `py-0 ${isAllVideoPage ? 'bg-primary-200' : ''}`,
-            }}
-            key="1"
-            startContent={
-              <div
-                className="flex w-fit items-center gap-3 font-medium"
-                onClick={handleYoutubeClick}
-              >
-                <Image src={'/icons/youtube.svg'} alt="youtube" width={18} height={18} />
-                <span>YouTube Automatization</span>
-              </div>
-            }
-          >
-            <div
-              onClick={handleAllVideoClick}
-              className="flex cursor-pointer gap-3 p-2 font-medium"
+    <>
+      {isMenuReady && (
+        <ul className="flex flex-col text-sm">
+          <li>
+            <Accordion
+              className="px-0"
+              itemClasses={{
+                base: 'data-[open=true]:bg-primary-100 rounded-medium px-0 py-2',
+              }}
+              defaultExpandedKeys={isYoutubePage || isAllVideoPage ? ['1'] : []}
             >
-              <Image src={'/icons/time.svg'} alt="youtube" width={18} height={18} />
-              <span>All video</span>
-            </div>
-          </AccordionItem>
-        </Accordion>
-      </li>
+              <AccordionItem
+                classNames={{
+                  indicator:
+                    'text-lg text-black-950 -rotate-90 data-[open=true]:rotate-90',
+                  trigger: `gap-1 w-full rounded-medium data-[open=true]:rounded-none p-2 ${isYoutubePage ? 'bg-primary-200' : ''}`,
+                  content: `py-0 ${isAllVideoPage ? 'bg-primary-200' : ''}`,
+                }}
+                key="1"
+                startContent={
+                  <div
+                    className="flex w-fit items-center gap-3 font-medium"
+                    onClick={handleYoutubeClick}
+                  >
+                    <Image
+                      src={'/icons/youtube.svg'}
+                      alt="youtube"
+                      width={18}
+                      height={18}
+                    />
+                    <span>YouTube Automatization</span>
+                  </div>
+                }
+              >
+                <div
+                  onClick={handleAllVideoClick}
+                  className="flex cursor-pointer gap-3 p-2 font-medium"
+                >
+                  <Image src={'/icons/time.svg'} alt="youtube" width={18} height={18} />
+                  <span>All video</span>
+                </div>
+              </AccordionItem>
+            </Accordion>
+          </li>
 
-      <MenuItem
-        href="?page=settings"
-        iconSrc="/icons/settings.svg"
-        label="Settings"
-        active={currentPage === 'settings'}
-      />
-    </ul>
+          <MenuItem
+            href="?page=settings"
+            iconSrc="/icons/settings.svg"
+            label="Settings"
+            active={currentPage === 'settings'}
+          />
+        </ul>
+      )}
+    </>
   )
 }
